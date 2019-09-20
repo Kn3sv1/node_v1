70d7a8c65c0409d98ab1a2964be0614ec2d5d5c4
Roman Satanovskyi <Roman.Satanovskyi@mindgeek.com>
2019-09-20 12:13:40 +0300

    INIT NODE

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import {test} from './addNumbers';
+
+//var test = require('./addNumbers');
+
+/*
+function addNumbers(a, b) {
+    return a + b;
+}
+*/
+console.log(test);
+//var sum = addNumbers(10, 15);
+//console.log('Sum of the two numbers is: ' + sum);
